09bc5bd4d1f0db778d404f5fe65ec2837e0e00c1
Alex Next <sprint_100m@mail.ru>
2016-10-18 13:29:19 +0300

    start

---
+++ UIView+TNRAnimationHelper.h
@@ -0,0 +1,95 @@
+//
+//  Created by Alexandr Panchenko on 14/10/16.
+//  Copyright (c) 2016 Thunderrise. All rights reserved.
+//
+//  Licensed under the MIT license: http://opensource.org/licenses/MIT
+//  Latest version can be found at https://github.com/...
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface UIView (TNRAnimationHelper)
+
+/**
+ Direction of flip animation.
+ */
+typedef NS_ENUM(NSUInteger, UIViewAnimationFlipDirection) {
+    UIViewAnimationFlipDirectionFromTop,
+    UIViewAnimationFlipDirectionFromLeft,
+    UIViewAnimationFlipDirectionFromRight,
+    UIViewAnimationFlipDirectionFromBottom,
+};
+
+/**
+  Direction of rotation animation.
+ */
+typedef NS_ENUM(NSUInteger, UIViewAnimationRotationDirection) {
+    UIViewAnimationRotationDirectionRight,
+    UIViewAnimationRotationDirectionLeft
+};
+
+/**
+  Shakes the view horizontally for a short period of time.
+ */
+- (void)tnr_shakeHorizontally;
+
+/**
+  Shakes the view vertically for a short period of time.
+ */
+- (void)tnr_shakeVertically;
+
+/**
+ Performs a pulsing scale animation on a view.
+
+ @param scale    - scale of animation.
+ @param duration - duration of the animation.
+ @param repeat   - pass YES for the animation to repeat.
+ */
+- (void)tnr_pulseToSize:(CGFloat)scale
+           duration:(NSTimeInterval)duration
+             repeat:(BOOL)repeat;
+
+/**
+ Performs a 3D-like flip animation of the view around center X or Y axis.
+ 
+ @param duration - total time of the animation.
+ @param direction - direction of the flip movement.
+ @param repeatCount - number of repetitions of the animation. Pass HUGE_VALF to repeat forever.
+ @param shouldAutoreverse - pass YES to make the animation reverse when it reaches the end.
+ */
+- (void)tnr_flipWithDuration:(NSTimeInterval)duration
+               direction:(UIViewAnimationFlipDirection)direction
+             repeatCount:(NSUInteger)repeatCount
+             autoreverse:(BOOL)shouldAutoreverse;
+
+
+/**
+ Performs a rotation animation of the view around its anchor point.
+ 
+ @param angle - end angle of the rotation. Pass M_PI * 2.0 for full circle rotation.
+ @param duration - total time of the animation.
+ @param direction - left or right direction of the rotation.
+ @param repeatCount - number of repetitions of the animation. Pass HUGE_VALF to repeat forever.
+ @param shouldAutoreverse - pass YES to make the animation reverse when it reaches the end.
+ */
+- (void)tnr_rotateToAngle:(CGFloat)angle
+             duration:(NSTimeInterval)duration
+            direction:(UIViewAnimationRotationDirection)direction
+          repeatCount:(NSUInteger)repeatCount
+          autoreverse:(BOOL)shouldAutoreverse;
+
+
+/**
+  Stops current animations.
+ */
+- (void)tnr_stopAnimation;
+
+
+/**
+  Checks if the view is being animated.
+ */
+- (BOOL)tnr_isBeingAnimated;
+
+
+@end
---
+++ UIView+TNRAnimationHelper.m
@@ -0,0 +1,121 @@
+//
+//  Created by Alexandr Panchenko on 14/10/16.
+//  Copyright (c) 2016 Thunderrise. All rights reserved.
+//
+//  Licensed under the MIT license: http://opensource.org/licenses/MIT
+//  Latest version can be found at https://github.com/...
+//
+
+#import "UIView+TNRAnimationHelper.h"
+
+@implementation UIView (TNRAnimationHelper)
+
+- (void)tnr_shakeHorizontally {
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.x"];
+    
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    animation.duration = 0.5f;
+    animation.values = @[@(-12), @(12), @(-8), @(8), @(-4), @(4), @(0)];
+    
+    [self.layer addAnimation:animation forKey:@"shake"];
+}
+
+
+- (void)tnr_shakeVertically {
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.y"];
+    
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    animation.duration = 0.5f;
+    animation.values = @[@(-12), @(12), @(-8), @(8), @(-4), @(4), @(0)];
+    
+    [self.layer addAnimation:animation forKey:@"shake"];
+}
+
+
+- (void)tnr_pulseToSize:(CGFloat)scale
+           duration:(NSTimeInterval)duration
+             repeat:(BOOL)repeat {
+    CABasicAnimation *pulseAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    
+    pulseAnimation.duration = duration;
+    pulseAnimation.toValue = [NSNumber numberWithFloat:scale];
+    pulseAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    pulseAnimation.autoreverses = YES;
+    pulseAnimation.repeatCount = repeat ? HUGE_VALF : 0;
+    
+    [self.layer addAnimation:pulseAnimation
+                      forKey:@"pulse"];
+    
+}
+
+
+- (void)tnr_flipWithDuration:(NSTimeInterval)duration
+               direction:(UIViewAnimationFlipDirection)direction
+             repeatCount:(NSUInteger)repeatCount
+             autoreverse:(BOOL)shouldAutoreverse {
+    NSString *subtype = nil;
+    
+    switch (direction) {
+        case UIViewAnimationFlipDirectionFromTop:
+            subtype = @"fromTop";
+            break;
+        case UIViewAnimationFlipDirectionFromLeft:
+            subtype = @"fromLeft";
+            break;
+        case UIViewAnimationFlipDirectionFromBottom:
+            subtype = @"fromBottom";
+            break;
+        case UIViewAnimationFlipDirectionFromRight:
+        default:
+            subtype = @"fromRight";
+            break;
+    }
+    
+    CATransition *transition = [CATransition animation];
+    
+    transition.startProgress = 0.0f;
+    transition.endProgress = 1.0f;
+    transition.type = @"flip";
+    transition.subtype = subtype;
+    transition.duration = duration;
+    transition.repeatCount = repeatCount;
+    transition.autoreverses = shouldAutoreverse;
+    
+    [self.layer addAnimation:transition
+                      forKey:@"spin"];
+}
+
+
+- (void)tnr_rotateToAngle:(CGFloat)angle
+             duration:(NSTimeInterval)duration
+            direction:(UIViewAnimationRotationDirection)direction
+          repeatCount:(NSUInteger)repeatCount
+          autoreverse:(BOOL)shouldAutoreverse {
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    
+    rotationAnimation.toValue = @(direction == UIViewAnimationRotationDirectionRight ? angle : -angle);
+    rotationAnimation.duration = duration;
+    rotationAnimation.autoreverses = shouldAutoreverse;
+    rotationAnimation.repeatCount = repeatCount;
+    rotationAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    
+    [self.layer addAnimation:rotationAnimation
+                      forKey:@"transform.rotation.z"];
+}
+
+
+- (void)tnr_stopAnimation {
+    [CATransaction begin];
+    [self.layer removeAllAnimations];
+    [CATransaction commit];
+    
+    [CATransaction flush];
+}
+
+
+- (BOOL)tnr_isBeingAnimated {
+    return [self.layer.animationKeys count];
+}
+
+
+@end
